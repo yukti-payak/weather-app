ba63a69062473b24a25963f79851562743dac38d
Yukti Payak <payakyukti2003@gmail.com>
2025-03-23 12:36:22 +0530

    Fix: Render Deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Allows external access
+    port: process.env.PORT || 3000, // Use Render's assigned port
+  }
+});
