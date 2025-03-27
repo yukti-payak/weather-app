059b1fdfcc3570ecf189dcaadffb3ff3350b232c
Yukti Payak <payakyukti2003@gmail.com>
2025-03-28 00:51:17 +0530

    k

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: true, // Allows access from external URLs
-    port: process.env.PORT || 5173, // Use Render's port or fallback to 5173
-  },
-  preview: {
-    host: true,
-    port: process.env.PORT || 5173,
-    allowedHosts: ["weathernexus.onrender.com"], // Add your Render domain
+    allowedHosts: ['weather-app-gyby.onrender.com'],
   },
 });
