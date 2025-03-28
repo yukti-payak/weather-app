0ddfb363a137b43d62d7d42bc44deef6c8b72773
Yukti Payak <payakyukti2003@gmail.com>
2025-03-28 13:08:27 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   port:process.env.PORT || 5173,
   host:"0.0.0.0",
   strictPort:true,
-  allowedHosts:[""]
- }
+  allowedHosts:["htweather-app-98v5.onrender.com"]
+ },
 });
 
