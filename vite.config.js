c683e65f3b90400fde5234dcf91e712847024d24
Yukti Payak <payakyukti2003@gmail.com>
2025-03-28 13:00:50 +0530

    f

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Allows access from external URLs
-    port: process.env.PORT || 5173, // Use Render's port or fallback to 5173
-  },
-  preview: {
-    host: true,
-    port: process.env.PORT || 5173,
-    allowedHosts: ["all"], // Add your Render domain
-  },
+ plugins:[react()],
+ server:{
+  port:process.env.PORT || 5173,
+  host:"0.0.0.0",
+  strictPort:true,
+  allowedHosts:[""]
+ }
 });
+
